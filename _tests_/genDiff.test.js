825e2fe88eece3fb99c971e4982c9077523b9f52
NRog <norogozin@gmail.com>
2022-08-22 19:25:29 +0400

    changed tests

--- _tests_/genDiff.test.js
+++ _tests_/genDiff.test.js
@@ -13,14 +13,16 @@ test('CorrectInputNestedJSON', () => {
         setting1: Value 1
       - setting2: 200
       - setting3: true
-      + setting3: null
+      + setting3: {
+            key: value
+        }
       + setting4: blah blah
       + setting5: {
             key5: value5
         }
         setting6: {
             doge: {
-              - wow: 
+              - wow: too much
               + wow: so much
             }
             key: value
@@ -49,6 +51,23 @@ test('CorrectInputNestedJSON', () => {
             }
         }
         fee: 100500
+    }
+    group4: {
+      - default: null
+      + default: 
+      - foo: 0
+      + foo: null
+      - isNested: false
+      + isNested: none
+      + key: false
+        nest: {
+          - bar: 
+          + bar: 0
+          - isNested: true
+        }
+      + someKey: true
+      - type: bas
+      + type: bar
     }
 }`;
 
@@ -63,14 +82,16 @@ test('CorrectInputYaml', () => {
         setting1: Value 1
       - setting2: 200
       - setting3: true
-      + setting3: null
+      + setting3: {
+            key: value
+        }
       + setting4: blah blah
       + setting5: {
             key5: value5
         }
         setting6: {
             doge: {
-              - wow: 
+              - wow: too much
               + wow: so much
             }
             key: value
@@ -100,82 +121,25 @@ test('CorrectInputYaml', () => {
         }
         fee: 100500
     }
+    group4: {
+      - default: null
+      + default: 
+      - foo: 0
+      + foo: null
+      - isNested: false
+      + isNested: none
+      + key: false
+        nest: {
+          - bar: 
+          + bar: 0
+          - isNested: true
+        }
+      + someKey: true
+      - type: bas
+      + type: bar
+    }
 }`;
 
   const result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
-  expect(result).toEqual(expected);
-});
-
-test('File1Empty', () => {
-  const expected = `{
-  + common: {
-        follow: false
-        setting1: Value 1
-        setting3: null
-        setting4: blah blah
-        setting5: {
-            key5: value5
-        }
-        setting6: {
-            key: value
-            ops: vops
-            doge: {
-                wow: so much
-            }
-        }
-    }
-  + group1: {
-        foo: bar
-        baz: bars
-        nest: str
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`;
-  const result = genDiff(getFixturePath('file3.json'), getFixturePath('file2.json'));
-  expect(result).toEqual(expected);
-});
-
-test('File2Empty', () => {
-  const expected = `{
-  - common: {
-        setting1: Value 1
-        setting2: 200
-        setting3: true
-        setting6: {
-            key: value
-            doge: {
-                wow: 
-            }
-        }
-    }
-  - group1: {
-        baz: bas
-        foo: bar
-        nest: {
-            key: value
-        }
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-}`;
-  const result = genDiff(getFixturePath('file1.json'), getFixturePath('file3.json'));
-  expect(result).toEqual(expected);
-});
-
-test('BothFilesEmpty', () => {
-  const expected = `{
-}`;
-  const result = genDiff(getFixturePath('file3.json'), getFixturePath('file3.json'));
   expect(result).toEqual(expected);
 });
