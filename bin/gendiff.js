4f061064dac1f3ddd5236c5fdae8cd4055276c2c
NRog <norogozin@gmail.com>
2022-08-25 23:06:49 +0400

    Changed cli options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,10 @@ cli.addHelpCommand(false);
 cli.helpOption(true);
 cli.version('1.001');
 cli.option('-f, --format <type>', 'output format');
-cli.arguments('<filepath1>', '<filepath1>');
-
+cli.arguments('<filepath1> <filepath2>');
+cli.action((filepath1, filepath2, options) => { 
+	console.log(genDiff(filepath1, filepath2, options.format));
+  });
 cli.parse(process.argv);
 
-console.log(genDiff(process.argv[2], process.argv[3], cli.opts().format));
+//console.log(genDiff(process.argv[2], process.argv[3], cli.opts().format));
