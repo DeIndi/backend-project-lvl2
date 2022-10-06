92f633b1464f46ef95886bef8b90853f06e38d12
NRog <norogozin@gmail.com>
2022-10-06 06:00:24 +0400

    added json resultFormat

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -15,24 +15,26 @@ const formatsToTest = [
 const resultFormats = [
   'stylish',
   'plain',
+  'json'
 ];
 
 const expectedOption = {
   stylish: readFileSync(getFixturePath('result_stylish.txt'), 'utf-8').trim(),
   plain: readFileSync(getFixturePath('result_plain.txt'), 'utf-8').trim(),
+  json: readFileSync(getFixturePath('result_json.txt'), 'utf-8').trim()
 };
 
 describe('gendiff', () => {
-  describe.each(formatsToTest)('для формата %s', (format) => {
+  describe.each(formatsToTest)('using format %s', (format) => {
     const fixture1 = getFixturePath(`file1.${format}`);
     const fixture2 = getFixturePath(`file2.${format}`);
-    it('результат по-умолчанию stylish', () => {
+    it('default result - stylish', () => {
       const expected = expectedOption.stylish;
       const result = genDiff(fixture1, fixture2);
       expect(result).toEqual(expected);
     });
 
-    it.each(resultFormats)('результат %s', (resultKey) => {
+    it.each(resultFormats)('result - %s', (resultKey) => {
       const result = genDiff(fixture1, fixture2, resultKey);
       const expected = expectedOption[resultKey];
       expect(result).toEqual(expected);
